4c8623543c62894c29f0ce5ea287a32f8bf4b569
sigursb <sigursb@stud.ntnu.no>
2025-09-19 09:23:05 +0200

    henting av filer og start på load_analysis

--- Kraftmarkeder - fleksibilitetsmodul/CINELDI_MV_reference_system-flexibility_course_NTNU_public/exercise_2_load_analysis.py
+++ Kraftmarkeder - fleksibilitetsmodul/CINELDI_MV_reference_system-flexibility_course_NTNU_public/exercise_2_load_analysis.py
@@ -28,7 +28,8 @@ import numpy as np
 
 # Location of (processed) data set for CINELDI MV reference system
 # (to be replaced by your own local data folder)
-path_data_set         = 'C:/Users/ivespe/Data_sets/CINELDI_MV_reference_system/'
+path_data_set         = r'C:\TET4565 Kraftmarkeder\Kraftmarkeder-Fordypningsemne\Kraftmarkeder - fleksibilitetsmodul\7703070\CINELDI_MV_reference_system_v_2023-03-06'
+
 
 filename_load_data_fullpath = os.path.join(path_data_set,'load_data_CINELDI_MV_reference_system.csv')
 filename_load_mapping_fullpath = os.path.join(path_data_set,'mapping_loads_to_CINELDI_MV_reference_grid.csv')
