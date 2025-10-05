95a4912c2604a8c3eceaea2e9b0d94bf3975da93
sigursb <sigursb@stud.ntnu.no>
2025-10-05 13:18:06 +0200

    Satt op deterministisk forslag til Benders-> Kan brukes til å løse stokastiske modellen

--- Kraftmarkeder - markedsmodul/Deterministic_benders.py
+++ Kraftmarkeder - markedsmodul/Deterministic_benders.py
@@ -24,59 +24,135 @@ M_conv = 3.6/1000
 E_conv = 0.657       
 WV_end = 52600.0 
 
-model = ConcreteModel()
-
-# Sets
-model.T1 = Set(initialize=T1)
-model.T2 = Set(initialize=T2)
 
 
-# Parameters: 
-model.p  = Param(model.T1|model.T2, initialize=p)
-model.I  = Param(model.T1, initialize=I)
-model.I_2 = Param(model.T2, initialize=I)
-model.V0 = Param(initialize=V0)
-model.Vmax = Param(initialize=Vmax)
-model.Pmax = Param(initialize=Pmax)
-model.Qmax = Param(initialize=Qmax)
-model.M_conv = Param(initialize=M_conv)
-model.E_conv = Param(initialize=E_conv)
-model.WV_end = Param(initialize=WV_end)
+# Creating master and sub:
+def create_master():
+    master = ConcreteModel()
+    master.T1 = Set(initialize=T1)
+    master.p = Param(master.T1, initialize={t: p[t] for t in T1})
+    master.I = Param(master.T1, initialize=I)
+    master.Pmax = Param(initialize=Pmax)
+    master.Qmax = Param(initialize=Qmax)
+    master.M_conv = Param(initialize=M_conv)
+    master.E_conv = Param(initialize=E_conv)
+    master.V0 = Param(initialize=V0)
+    master.Vmax = Param(initialize=Vmax)
+
+    # First stage variables
+    master.x = Var(master.T1, domain=NonNegativeReals, bounds=(0,Pmax))
+    master.Q = Var(master.T1, domain=NonNegativeReals, bounds=(0,Qmax))
+    master.V = Var(master.T1, domain=NonNegativeReals, bounds=(0,Vmax))
+
+    # Benders cut variable
+    master.alfa = Var(domain=Reals, initialize = 0)
+
+    # Constraints
+    def reservoir_day1_rule(m,t):
+        if t == 1:
+            return m.V[t] == m.V0 + m.M_conv*(m.I[t] - m.Q[t])
+        return m.V[t] == m.V[t-1] + m.M_conv*(m.I[t] - m.Q[t])
+    master.res_day1 = Constraint(master.T1, rule=reservoir_day1_rule)
+
+    def prod_day1_rule(m,t):
+        return m.x[t] == m.E_conv*m.M_conv*m.Q[t]*1000
+    master.prod_day1 = Constraint(master.T1, rule=prod_day1_rule)
+
+    def Obje_1(m):
+        return sum(m.p[t]*m.x[t] for t in m.T1) + m.alfa
+
+    # Objective function
+    master.obj = Objective(rule=Obje_1, sense=maximize)
+
+    return master
+
+def create_sub(V_24):
+    sub = ConcreteModel()
+    sub.T2 = Set(initialize=T2)
+    sub.p = Param(sub.T2, initialize={t: p[t] for t in T2})
+    sub.I_2 = Param(sub.T2, initialize={t: I_2[t] for t in T2})
+    sub.Pmax = Param(initialize=Pmax)
+    sub.Qmax = Param(initialize=Qmax)
+    sub.M_conv = Param(initialize=M_conv)
+    sub.E_conv = Param(initialize=E_conv)
+    sub.WV_end = Param(initialize=WV_end)
+    
+
+    # Second stage variables
+    sub.x_s = Var(sub.T2, domain=NonNegativeReals, bounds=(0,Pmax))
+    sub.Q_s = Var(sub.T2, domain=NonNegativeReals, bounds=(0,Qmax))
+    sub.V_s = Var(sub.T2, domain=NonNegativeReals, bounds=(0,Vmax))
+
+    # Constraints
+    def reservoir_day2_rule(m,t):
+        if t == 25:
+            return m.V_s[t] == V_24 + m.M_conv*(m.I_2[t] - m.Q_s[t])
+        return m.V_s[t] == m.V_s[t-1] + m.M_conv*(m.I_2[t] - m.Q_s[t])
+    sub.res_day2 = Constraint(sub.T2, rule=reservoir_day2_rule) 
+
+    def prod_day2_rule(m,t):
+        return m.x_s[t] == m.E_conv*m.M_conv*m.Q_s[t]*1000
+    sub.prod_day2 = Constraint(sub.T2, rule=prod_day2_rule)
+
+    def Obje_2(m):
+        return sum(m.p[t]*m.x_s[t] for t in m.T2) + m.WV_end*m.V_s[48]
+
+    # Objective function
+    sub.obj = Objective(rule=Obje_2, sense=maximize)
+
+    return sub
 
 
-# Decision variables
-model.x  = Var(model.T1, domain=NonNegativeReals, bounds=(0,Pmax))
-model.Q  = Var(model.T1, domain=NonNegativeReals, bounds=(0,Qmax))
-model.V  = Var(model.T1, domain=NonNegativeReals, bounds=(0,Vmax))
-
-model.x_2 = Var(model.T2, domain=NonNegativeReals, bounds=(0,Pmax))
-model.Q_2 = Var(model.T2, domain=NonNegativeReals, bounds=(0,Qmax))
-model.V_2 = Var(model.T2, domain=NonNegativeReals, bounds=(0,Vmax))
+# Defining Benders:
+# Master problem: First stage variabel with the given constraints. 
+# The subproblem: The second stage variables with the given constraints. 
 
 
-# Constraints
-def reservoir_day1_rule(m,t):
-    if t == 1:
-        return m.V[t] == m.V0 + m.M_conv*(m.I[t] - m.Q[t])
-    return m.V[t] == m.V[t-1] + m.M_conv*(m.I[t] - m.Q[t])
-model.res_day1 = Constraint(model.T1, rule=reservoir_day1_rule)
+def Benders_algo():
+    master = create_master()
+    UB = 1e5
+    LB = -1e5
+    tol = 1e-1
+    it = 0
+    cuts = []
+    while abs(UB-LB)>tol:
+        it += 1
+        opt = SolverFactory('glpk')
+        # Solve master and ensure solution is loaded into the model
+        # Prevent master from being unbounded at the very first iteration: fix alfa=0
+        if it == 1:
+            master.alfa.fix(0)
 
-def reservoir_day2_rule(m,t):
-    if t == 25:
-        return m.V_s[t] == m.V[24] + m.M_conv*(m.I_s[t] - m.Q_s[t])
-    return m.V_s[t] == m.V_s[t-1] + m.M_conv*(m.I_s[t] - m.Q_s[t])
-model.res_day2 = Constraint(model.T2, rule=reservoir_day2_rule) 
+        # Solving the master problem and extracting the value to send to sub_problem:
+        res_master = opt.solve(master, tee=False)
+        master.dual = Suffix(direction=Suffix.IMPORT)
+        V_24 = value(master.V[24])
+    
 
-def prod_day1_rule(m,t):
-    return m.x[t] == m.E_conv*m.M_conv*m.Q[t]*1000
-model.prod_day1 = Constraint(model.T1, rule=prod_day1_rule)
+        # Create and solve subproblem for this V_24
+        sub = create_sub(V_24)
+        sub.dual = Suffix(direction=Suffix.IMPORT)
+        res_sub = opt.solve(sub, tee=False)
+    
 
-def prod_day2_rule(m,t):
-    return m.x_s[t] == m.E_conv*m.M_conv*m.Q_s[t]*1000
-model.prod_day2 = Constraint(model.T2, rule=prod_day2_rule)
+        # Compute bounds: LB is master objective (with current alfa), UB is first-stage profit + true second-stage objective
+        LB = value(master.obj)
+        first_stage_profit = sum(value(master.p[t]) * value(master.x[t]) for t in master.T1)
+        second_stage_profit = value(sub.obj)
+        UB = first_stage_profit + second_stage_profit
+        print(f'LB: {LB}, UB: {UB}, Gap: {abs(UB-LB)}')
 
-def obj_rule(m):
-    term1 = sum(m.p[t]*m.x[t] for t in m.T1)
-    term2 = sum(m.p[t]*m.x_s[t] for t in m.T2) + m.WV_end*m.V_s[48]
+        # Get dual of reservoir constraint at time 25 (connecting variable)
+        dual_lambda = sub.dual[sub.res_day2[25]]
 
+        # Add Benders optimality cut: alfa <= phi_k + pi_k*(V24 - V24_k)
+        phi_k = value(sub.obj)
+        expr = master.alfa <= phi_k + dual_lambda * (master.V[24] - V_24)
+        cname = f"cut_{it}"
+        setattr(master, cname, Constraint(expr=expr)) # Appends the cut to master
+        cuts.append(cname)
 
+    print(f"Covergence after {it} iterations")
+    print(f"Objective function value = {LB:.4f}")
+
+Benders_algo()
